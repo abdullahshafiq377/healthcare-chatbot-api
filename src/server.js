da8b3a57be279a77fabd6d897c102aef7ddce02d
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2025-02-18 20:52:37 +0500

    removed auth middleware

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ app.use(
 		                                 }),
 		        cookie: {
 			        httpOnly: true,
-			        secure: false,
+			        secure: true,
 			        sameSite: 'none',
 			        maxAge: 1000 * 60 * 60 * 24, // 1 day
 		        },
