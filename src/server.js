14aca1a9f03138aeda9452d964f66d077d177ce0
Abdullah Shafiq <abdullahshafiq377@gmail.com>
2025-02-19 20:38:35 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ app.use(
 		        cookie: {
 			        httpOnly: true,
 			        secure: true,
-			        sameSite: 'lax',
+			        sameSite: 'none',
 			        maxAge: 1000 * 60 * 60 * 24, // 1 day
 		        },
 	        })
